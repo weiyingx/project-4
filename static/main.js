4da6f44a03922c4384c24e6d3703f9fd6b407db7
WEI YING ENG <weiyingeng@WEIs-MacBook-Pro.local>
2016-11-01 22:46:33 +0800

    add booking form

--- static/main.js
+++ static/main.js
@@ -6,5 +6,19 @@ $(document).ready(function() {
   });
   $('.ui.dropdown')
   .dropdown()
+  ;
+$('.ui.form')
+  .form({
+    fields: {
+      name     : 'empty',
+      gender   : 'empty',
+      username : 'empty',
+      password : ['minLength[6]', 'empty'],
+      skills   : ['minCount[2]', 'empty'],
+      terms    : 'checked'
+    }
+  });
+  $('.ui.checkbox')
+  .checkbox()
 ;
 })
