4ccc4a52bae4907fa2a31cb589b96f3402d39c38
WEI YING ENG <weiyingeng@WEIs-MBP.lan>
2016-11-01 17:35:01 +0800

    fixed semantic script

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ app.get('/berjaya', function(req, res) {
   res.render('berjaya');
 });
 
+app.get('/paya', function(req, res) {
+  res.render('paya');
+});
+
 app.get('/panuba', function(req, res) {
   res.render('panuba');
 });
@@ -58,19 +62,56 @@ app.get('/dcoconut', function(req, res) {
   res.render('dcoconut');
 });
 
+
+var numUsers = 0;
+
 io.on('connection', function(socket){
+  var addedUser = false;
   console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('new message', function (data) {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
 
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 
 http.listen(port, function(){
---
+++ static/main.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+
+$(document).ready(function() {
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+  $('.ui.dropdown')
+  .dropdown()
+;
+})
